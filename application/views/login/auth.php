63a6fa87a25c625f3245bd166d1c8b2b625fbe6f
NanoNao <benjaminsimon22@msn.com>
2017-02-10 10:24:48 +0100

    Modification Auth
    Ajout Model et modif controler

--- application/views/login/auth.php
+++ application/views/login/auth.php
@@ -1,5 +1,12 @@
 
 <!DOCTYPE html>
+
+<?php
+if (isset($this->session->userdata['logged_in'])) {
+
+    redirect('back');
+}
+?>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -40,51 +47,10 @@
     resize: none;
   }
   </style>
-  <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Brand</a>
-      </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Link</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-        <style media="screen">
-
-        </style>
-        <ul class="nav navbar-nav navbar-right">
-          <li><button type="submit" class="btn btn-success " style="margin-top:8px;"><span class="glyphicon glyphicon-user"></span>  Connecter</button></li>
-
-
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
-  <!--Fin Navbar-->
 
   <div class ="row">  <!-- Bouton connectez vous -->
+    <?php echo form_open('auth/user_login_process'); // debut du formulaire ?>
     <div class="col-md-6 col-md-offset-3 well">
       <h2 class="text-center">Connectez-vous</h2>
     </div>
@@ -101,7 +67,23 @@
               <p><input type="password" name="password" id="password" placeholder="**********"/></p>
               <button type="button" name="button"  class ="btn-primary btn btn-block">Envoyer</button>
             </div>
+            <div id="main">
+                                   <div id="login">
 
+                                       <?php
+                                      // echo "<div class='error_msg'>";
+                                       if (isset($error_message)) {
+                                            echo '<div class="alert alert-danger" role="alert">Identifiants invalide</div>';
+                                       }
+                                       echo validation_errors();
+                                      // echo "</div>";
+                                       ?>
+                                       <a href="<?php echo base_url() ?>auth/user_registration_show">Inscrivez-vous</a>
+
+
+                                       <?php echo form_close(); ?>
+                                   </div>
+                               </div>
           </div>
         </div>
       </div>
