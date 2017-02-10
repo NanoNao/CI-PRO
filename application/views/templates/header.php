fe62d24c69c2bdc574240b8b80a8a9050f5056f3
Skquik <nalletpaul@gmail.com>
2017-02-10 09:09:48 +0100

    Integration de views
    Navbar, Footer, Header

---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/bootstrap/css/bootstrap.min.css">
+    <title>Urapeda ASSURANCE</title>
+  </head>
+<body>
